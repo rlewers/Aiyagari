3c757e2adef38ffa49ca8b7bac26543ec0f764d8
rlewers <rlewers@ucsd.edu>
2023-09-08 16:10:20 -0700

    started simulation method to get K

--- Aiyagari.py
+++ Aiyagari.py
@@ -10,6 +10,8 @@ Created on Thu Sep  7 12:35:35 2023
 
 import numpy as np
 import math
+import random
+from scipy.interpolate import interp1d
 
 # Model Parameters
 beta = 0.96 # discount rate
@@ -46,6 +48,27 @@ agrid = np.linspace(a_min, a_max, na)
 tol_for_vfi = 1e-6
 tol_for_loop = 1e-6
 
+# Setting up some stuff to use simulation method 
+T = 10000 # this will be the number of agents 
+burn_in = 5000 
+np.random.seed(1020) # set this so simulation results will be the same each time
+ysimul = np.zeros(T) # col vector w/ 10000 rows 
+# don't understand why he does this
+# I guess it sets the first agent to low productivity
+ysimul[0]=ygrid[0] 
+
+# filling the vector of productivity realizations for all agents in the economy 
+# not sure why he does it this way...
+for t in range(1, T):
+    #prestate = ysimul[t-1] == ygrid 
+    prestate = np.where(ysimul[t-1] == ygrid)[0][0]
+    generate = np.random.uniform(0, 1)
+    if generate < TransM[prestate][1]: # bit confused by this if condition
+        ysimul[t] = ygrid[0]
+    else:
+        ysimul[t] = ygrid[1]
+        
+
 # VFI Set up 
 # discounted maximized lifetime value given assets and productivity
 V = np.zeros([na, ny]) 
@@ -60,13 +83,13 @@ the representative firm's problem, and the defintion of a stationary equilibrium
     1. Guess K
     2. Using firm's FOCs, solve for r and w
     3. Given r and w, determine HH optimal savings (use vfi)
-    4. From HH savings, aggregate up to get K
+    4. From HH savings, aggregate up to get K (DK uses simulation method)
     5. If this K agrees w/ our initial guess, we are done. Otherwise, repeat.'''
 
 Kinterv = [2, 6]
 diff_in_main = 1 # tells us whether our guess of K is close enough
 
-for i in range(10000):
+for i in range(10000): # I guess this is like setting max iterations?
     K=(Kinterv[0]+Kinterv[1])/2 # guess aggregate capital level 
     # representative firm's optimality conditions determine R and W
     R = (alpha)*(K/L)**(alpha-1)+1-delta 
@@ -76,25 +99,55 @@ for i in range(10000):
     while diff_in_valfun > tol_for_vfi:
         
         Vnew = V
-        V = np.zeros(na, ny)
-        pol_func = np.zeros(na, ny)
-        con = np.zeros(na, ny)
+        V = np.zeros([na, ny])
+        pol_func = np.zeros([na, ny])
+        con = np.zeros([na, ny])
         
         # loop over productivity levels 
-        for y_ind in len(ny)-1:
+        for y_ind in range(ny):
             # loop over asset choices 
-            for a_ind in len(na)-1:
-                wealth = R*agrid(a_ind) + W*ygrid(y_ind)
+            for a_ind in range(na):
+                wealth = R*agrid[a_ind] + W*ygrid[y_ind]
                 wealth = [wealth]*na
                 max1 = max(wealth-agrid)
                 
                 # the below is a 200x1 row vector. It is like 1 raw in Carlos's program
-                V_max=math.log(max(max1, 1.0e-15))+beta*(np.matmul(Vnew, TransM[y_ind]))[:]
+                V_max=math.log(max(max1, 1.0e-15))+beta*(np.matmul(Vnew, TransM[y_ind, :]))
                 
-                # place the maximum of V_max into the correct spot in the V matrix
-                V[a_ind][y_ind]=max(V_max)
+                # place stuff into the correct spot in the value, policy, and consumption matrices
+                V[a_ind, y_ind] = np.max(V_max)
+                temp = np.argmax(V_max)
+                pol_func[a_ind, y_ind] = agrid[temp]
+                con[a_ind, y_ind] = wealth - pol_func[a_ind, y_ind]
         
         diff_in_valfun = max(max(abs(V-Vnew))) # thanks Elke!
+        
+    # Do simulation method to get K 
+    
+    # get a finer agrid 
+    a_gridfiner = np.linspace(a_min,a_max,int(1.5*na))
+    
+    # Use linear interpolation to get finer policy function corresponding 
+    # to our finer agrid
+    # we need to specify axis=0 b/c rows in the policy function array 
+    # correspond to agrid (asset) values
+    # I think this is the corresponding python syntax to do what is in DK's code
+    # but I'm not 100% sure
+    f = interp1d(agrid, pol_func, axis=0, kind='linear')
+    pol_func_finer = f(a_gridfiner)
+    
+    # initialize a vector to put our simulated asset values into
+    a_sim = np.zeros([T+1,1])
+    a_sim[0]=a_gridfiner[0]
+    
+    # Loop to update a_sim
+    # need to better understand what is going on here
+    for t in range(T):
+        y_idx = np.where(ysimul[t] == ygrid)[0][0]  # Find the index where ysimul(t) matches ygrid
+        a_sim[t + 1] = interp1d(a_gridfiner, pol_func_finer[:, y_idx], kind='linear', fill_value='extrapolate')(a_sim[t])
+        
+    a_sim = a_sim[1+burn_in:T]
+    y_sim = ysimul[1+burn_in:T]
 
         
         
